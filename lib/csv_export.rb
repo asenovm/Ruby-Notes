8e4cb9a5fa78190c2ff44d29f022fddc1d4c4819
Martin Asenov <asenov.m@gmail.com>
2012-02-05 22:07:56 +0200

    added csv export

--- lib/csv_export.rb
+++ lib/csv_export.rb
@@ -1,3 +1,19 @@
 class CsvExport
-  
+
+  CSV_VALUE_SEPARATOR = ', '
+
+  def self.export(note_list)
+    res = String.new
+    note_list.each do |note|
+      res += export_note(note)
+      res += '\n'
+    end
+    res
+  end
+
+  private
+  def self.export_note(note)
+    res = note.id + CSV_VALUE_SEPARATOR + note.get_tags_as_string + CSV_VALUE_SEPARATOR
+    res += note.description + CSV_VALUE_SEPARATOR + note.due_date
+  end
 end
