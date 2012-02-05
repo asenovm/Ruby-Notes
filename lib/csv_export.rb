74b5fb5b658cdb5ca1a3e65ba51813796dbd3f6b
Martin Asenov <asenov.m@gmail.com>
2012-02-06 00:15:22 +0200

    console output is now almost properly working

--- lib/csv_export.rb
+++ lib/csv_export.rb
@@ -14,6 +14,6 @@ class CsvExport
   private
   def self.export_note(note)
     res = note.id + CSV_VALUE_SEPARATOR + note.get_tags_as_string + CSV_VALUE_SEPARATOR
-    res += note.description + CSV_VALUE_SEPARATOR + note.due_date
+    res += '"' + note.description + '"' + CSV_VALUE_SEPARATOR + note.due_date
   end
 end
