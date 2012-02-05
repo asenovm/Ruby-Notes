91421e7c7049b2985d0118f669484f2705ee3e34
Martin Asenov <asenov.m@gmail.com>
2012-02-05 21:08:57 +0200

    specs for the note model

---
+++ specs/note_spec.rb
@@ -0,0 +1,91 @@
+describe Note do
+  describe 'initialization' do
+    it 'can be initialized with no args' do
+      note = Note.new
+    end
+  end
+
+  describe'tags work properly' do
+    it 'can have a tag' do
+        note = Note.new
+        note.add_tag('todo')
+    end
+    
+    it 'cannot have one tag multiple times' do
+      note = Note.new
+      note.add_tag('multiple')
+      note.add_tag('multiple')
+      note.tags.count.should eq 1
+    end
+
+    it 'can have many tags' do
+      note = Note.new
+      note.add_tag('to do')
+      note.add_tag('not to do')
+      note.add_tag('third tag')
+      note.tags.count.should eq 3
+    end
+
+    it 'can have tags removed' do
+      note = Note.new
+      note.add_tag('to do')
+      note.remove_tag('to do')
+      note.tags.count.should eq 0
+    end
+
+    it 'can have unexisting tags removed' do
+      note = Note.new
+      note.remove_tag('to do')
+      note.tags.count.should eq 0
+    end
+  end
+
+  describe 'description' do
+    it 'can be set a description' do
+      note = Note.new
+      note.description = 'foobar'
+    end
+
+    it 'can have description removed' do
+      note = Note.new
+      note.description = "foo bar"
+      note.remove_description
+      note.description.should eq nil
+    end
+
+    it 'can have description overriden' do
+       note = Note.new
+       note.description = 'foobar'
+       note.description = 'new description'
+       note.description.should eq 'new description'
+    end
+  end
+
+  describe 'to string output' do
+    it 'can have tags exported to string' do
+        note = Note.new
+        note.add_tag 'to do'
+        note.get_tags_as_string.should eq 'to do'
+    end
+
+    it 'can have multiple tags exported as string correctly' do
+      note = Note.new
+      note.add_tag 'to do'
+      note.add_tag 'not to do'
+      note.add_tag 'something else'
+      note.get_tags_as_string.should eq 'to do, not to do, something else'
+    end
+
+    it 'adding and removing tags dont mess with tags output' do
+      note = Note.new
+      note.add_tag 'to do'
+      note.add_tag 'to be done'
+      note.add_tag 'music'
+      note.add_tag 'something else'
+      note.remove_tag 'to do'
+      note.remove_tag ' music '
+      note.get_tags_as_string.should eq 'to be done, music, something else'
+    end
+  end
+
+end
