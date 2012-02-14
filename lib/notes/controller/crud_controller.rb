5a95f25428c8ac0d85d28e951a4012d7e919975f
Martin Asenov <asenov.m@gmail.com>
2012-02-14 21:01:44 +0200

    crud controller should be working now

--- lib/notes/controller/crud_controller.rb
+++ lib/notes/controller/crud_controller.rb
@@ -4,5 +4,35 @@ module Notes
       @options = options
       @db_controller = DatabaseController.new
     end
+
+    def perform_action
+      if @options[:add_note]
+        insert
+      elsif @options[:remove_note]
+        remove
+      elsif @options[:update_note]
+        update
+      elsif @options[:find_note]
+        find
+      end
+    end
+
+    private
+    def insert
+      @db_controller.insert Note.new @options
+    end
+
+    def remove
+      @db_controller.remove Criteria.new @options
+    end
+
+    def update
+      @db_controller.update Note.new @options, Criteria.new @options
+    end
+
+    def find
+      @db_controller.find Criteria.new @options
+    end
+
   end
 end
