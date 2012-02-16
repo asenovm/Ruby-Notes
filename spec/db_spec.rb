3f012e1b18370de465405b9c628f2abae0287cfa
Martin Asenov <asenov.m@gmail.com>
2012-02-16 22:36:05 +0200

    moved the inserting of the note to the db in the setup method

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -2,6 +2,8 @@ describe 'works with db' do
 
   before do
     @db_controller = Notes::DatabaseController.new 'spec'
+    @note = Notes::Note.new({Notes::Options::TAG => 'to do', Notes::Options::DESCRIPTION => 'description', Notes::Options::DUE_DATE => '16.02.2012'})
+    @db_controller.insert @note
   end
 
   after do
@@ -9,8 +11,6 @@ describe 'works with db' do
   end
 
   it 'can insert and find notes notes successfully' do
-    note = Notes::Note.new({Notes::Options::TAG => 'to do', Notes::Options::DESCRIPTION => 'description', Notes::Options::DUE_DATE => '16.02.2012'})
-    @db_controller.insert note
     query_result = @db_controller.find([Notes::Criteria.new(Notes::Options::TAG.to_s, 'to do'), Notes::Criteria.new(Notes::Options::DESCRIPTION.to_s, 'description'), Notes::Criteria.new(Notes::Options::DUE_DATE.to_s, '16.02.2012')])
     result_length = 0
     query_result.each do |result|
@@ -20,8 +20,6 @@ describe 'works with db' do
   end
 
   it 'can update note' do
-    note = Notes::Note.new({Notes::Options::TAG => 'to do', Notes::Options::DESCRIPTION => 'description', Notes::Options::DUE_DATE => '16.02.2012'})
-    @db_controller.insert note
     inserted_note = @db_controller.find_one([Notes::Criteria.new(Notes::Options::TAG.to_s, 'to do')])
     @db_controller.update(Notes::Note.new({Notes::Options::ID => inserted_note[Notes::Options::TOKEN.to_s]}), [Notes::Criteria.new(Notes::Options::TAG.to_s, 'other')])
     query_result = @db_controller.find([Notes::Criteria.new(Notes::Options::TAG.to_s, 'other')])
@@ -33,8 +31,6 @@ describe 'works with db' do
   end
 
   it 'can remove note' do
-    note = Notes::Note.new({Notes::Options::TAG => 'to do', Notes::Options::DESCRIPTION => 'description', Notes::Options::DUE_DATE => '16.02.2012'})
-    @db_controller.insert note
     criterias = [Notes::Criteria.new(Notes::Options::TAG.to_s, 'to do')]
     @db_controller.remove criterias
     query_result = @db_controller.find criterias
