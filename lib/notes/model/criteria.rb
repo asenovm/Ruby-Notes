b57d74a072a237797290a92c2450b02513130dcf
Martin Asenov <asenov.m@gmail.com>
2012-02-17 14:56:09 +0200

    tags are now properly working, some refactoring is due

--- lib/notes/model/criteria.rb
+++ lib/notes/model/criteria.rb
@@ -7,6 +7,14 @@ module Notes
       end
 
       def to_hash
+        if @value.kind_of? Array
+          result_value = ''
+          @value.each do |value|
+            result_value += value
+            result_value += ', '
+          end
+          return {@name => result_value.chop!.chop!}
+        end
         {@name => @value}
       end
     end
